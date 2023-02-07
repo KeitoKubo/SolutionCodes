0a9fe73f73313a277311d2cb62272df445f0ffee
KeitoKubo <89447369+KeitoKubo@users.noreply.github.com>
2023-02-07 19:06:34 +0900

    0207 afternoon

---
+++ past contests/ABC287/c.cpp
@@ -0,0 +1,146 @@
+#define _USE_MATH_DEFINES
+#include <iostream>                         // cout, endl, cin
+#include <cstdio>                           // printf, scanf
+#include <string>                           // string, to_string, stoi
+#include <algorithm>                        // min, max, swap, lower_bound, upper_bound
+// & stable_sort, sort, reverse
+#include <cmath>                            //sin(rad),cos,tan, abs, pow, sqrt, cbrt,  exp, log, log10, log2
+#include <utility>                          // pair, make_pair
+#include <map>                              // map
+#include <set>                              // set
+#include <vector>                           // vector
+#include <queue>                            // queue, priority_queue
+#include <stack>                            // stack
+#include <list>                             // list
+#include <deque>                            // deque
+#include <iomanip>
+#include <numeric>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef vector<int> vi;
+typedef vector<string> vs;
+typedef pair<int, int> pii;
+typedef pair<string, int> psi;
+typedef pair<int, string> pis;
+typedef set<int> si;
+typedef map<string, int> msi;
+typedef map<string, bool> msb;
+typedef priority_queue<int> pqi;
+typedef stack<int> sti;
+typedef queue<int> qi;
+
+#define infi 2147483647
+#define infl 9223372036854775806
+#define MOD  1000000007
+#define EPS 0.0000000001
+#define rep(i,n) for(int i=0;i<(int)n;i++)
+#define repa(i,n) for(int i=1;i<=(int)n;i++)
+#define irep(i,n) for(int i=(int)n-1;i>=0;i--)
+
+//Union Find Tree
+class DisjointSet {
+public:
+	vi rank, p, child; //rankはその木の深さ、pは親ノード
+
+	DisjointSet() {}
+	DisjointSet(int size) {
+		rank.resize(size + 1, 0);
+		p.resize(size + 1, 0);
+		child.resize(size + 1, 0);
+		repa(i, size) makeSet(i);
+	}
+
+	void makeSet(int x) {
+		p[x] = x;
+		rank[x] = 0;
+		child[x] = 1;
+	}
+
+	int findSet(int x) {
+		if (x != p[x]) p[x] = findSet(p[x]);
+		return p[x];
+	}
+
+	int howmanychilds(int x) {
+		return child[x];
+	}
+
+	bool same(int x, int y) {
+		if (findSet(x) == findSet(y)) return true;
+		else return false;
+	}
+
+	void link(int x, int y) {
+		if (rank[x] > rank[y]) {
+			p[y] = x;
+			child[x] += child[y];
+		}
+		else {
+			p[x] = y; child[y] += child[x];
+			if (rank[x] == rank[y]) rank[y]++;
+		}
+	}
+
+	void unite(int x, int y) {
+		link(findSet(x), findSet(y));
+	}
+};
+
+//sort vector<pii> by second element
+bool compare_by_b(pair<int, int> a, pair<int, int> b) {
+	if (a.second != b.second) {
+		return a.second < b.second;
+	}
+	else {
+		return a.first < b.first;
+	}
+}
+
+//---------------------------------------------------
+const int max_n = 2e5;
+int node_weight[max_n + 5];
+
+int main() {
+	//(void)scanf("%d",& );
+	//(void)scanf("%d%d",& ,& );
+	int n, m; cin >> n >> m;
+	repa(i, n) node_weight[i] = 0;
+	DisjointSet ds = DisjointSet(n);
+	rep(i, m) {
+		int x, y; (void)scanf("%d%d", &x, &y);
+		node_weight[x]++; node_weight[y]++;
+		ds.unite(x - 1, y - 1);
+	}
+
+	int node_weight_one = 0, node_weight_two = 0;
+	bool not_exist = false;
+	repa(i, n) {
+		if (node_weight[i] == 1) node_weight_one++;
+		else if (node_weight[i] == 2) node_weight_two++;
+		else not_exist = true;
+	}
+
+	if (not_exist) {
+		cout << "No" << endl;
+	}
+	else {
+		if (node_weight_one == 2 && node_weight_two == n - 2) {
+			bool flag = true;
+			rep(i, n-1) {
+				if (!ds.same(i, i + 1)) { flag = false; break; }
+			}
+			if (flag) cout << "Yes" << endl;
+			else cout << "No" << endl;
+		}
+		else cout << "No" << endl;
+	}
+
+	return 0;
+}
+
