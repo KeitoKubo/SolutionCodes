7abc4add301261b8472b0abec7b2f29e2ef46d0a
KeitoKubo <89447369+KeitoKubo@users.noreply.github.com>
2023-02-17 20:27:47 +0900

    ABC289 F passed!!

--- past contests/ABC251-300/ABC289/f.cpp
+++ past contests/ABC251-300/ABC289/f.cpp
@@ -42,7 +42,7 @@ typedef queue<int> qi;
 #define rep(i,n) for(int i=0;i<(int)n;i++)
 #define repa(i,n) for(int i=1;i<=(int)n;i++)
 #define irep(i,n) for(int i=(int)n-1;i>=0;i--)
-#define repl(i,n) for(ll i=0;i<(ll)n;i++)
+#define repl(i,n) for(ll i=0;i<(ll)n;i+=(ll)1)
 
 //Union Find Tree
 class DisjointSet {
@@ -104,7 +104,7 @@ bool compare_by_b(pair<int, int> a, pair<int, int> b) {
 }
 
 //---------------------------------------------------
-
+const ll maxi = 990000;
 
 int main() {
 	//(void)scanf("%d",& );
@@ -112,81 +112,20 @@ int main() {
 	ll sx, sy, tx, ty; cin >> sx >> sy >> tx >> ty;
 	ll a, b, c, d; cin >> a >> b >> c >> d;
 
-	//必ず10^6回もしくは10^6-1回移動させることを考える
 	vector<ll> vec_x, vec_y;
 	int zeroflag_x = 0, zeroflag_y = 0;
 	if ((sx + tx) % 2 == 1 || ((sy + ty) % 2 == 1)) {
 		cout << "No" << endl; return 0;
 	}
-	/*else if (tx + sx <= 1000000 * b - 499999 * 2 * a && ty + sy <= 500000 * 2 * d - 499999 * 2 * c) { //奇数回操作
-		//x部分
-		if (b == a) {
-			if (sx + tx != 2*b) { cout << "No" << endl; return 0; }
-			else {
-				rep(i, 1000000 - 1) vec_x.push_back(b);
-			}
-		}
-		else {
-			ll btoa_x = (tx + sx) / (2 * (b - a)); //b,aと2回の対象移動をする回数
-			ll mod_x = (tx + sx) % (2 * (b - a));
-			mod_x /= 2;
-			if (mod_x < a) {
-				while (mod_x < a) {
-					if (btoa_x <= 0) { cout << "called 2x No" << endl; return 0; }
-					mod_x += b - a; btoa_x--;
-				}
-			}
-			ll remain = 1000000 - 1 - 1 - 2 * btoa_x; //残りの移動回数、必ず偶数になるのでa,aと対象移動をさせればよい
-			vec_x.push_back(mod_x);
-			rep(i, btoa_x) {
-				vec_x.push_back(b); vec_x.push_back(a);
-			}
-			rep(i, remain) {
-				vec_x.push_back(a);
-			}
-		}
-
-		//y部分
-		if (c == d) {
-			if (sy+ty != 2*d) { cout << "called 1y No" << endl; return 0; }
-			else {
-				rep(i, 1000000 - 1) vec_y.push_back(d);
-			}
-		}
-		else {
-			ll btoa_y = (ty + sy) / (2 * (d - c)), mod_y = (ty + sy) % (2 * (d - c));
-			mod_y /= 2;
-			if (mod_y < c) {
-				while (mod_y < c) {
-					if (btoa_y <= 0) { cout << "called 2y No" << endl; return 0; }
-					mod_y += d - c; btoa_y--;
-				}
-			}
-			ll remain = 1000000 - 1 - 1 - 2 * btoa_y; //残りの移動回数、必ず偶数になるのでa,aと対象移動をさせればよい
-			vec_y.push_back(mod_y);
-			rep(i, btoa_y) {
-				vec_y.push_back(d); vec_y.push_back(c);
-			}
-			rep(i, remain) {
-				vec_y.push_back(c);
-			}
-		}
-
-		cout << "Yes" << endl;
-		rep(i, 1000000 - 1) {
-			printf("%d %d\n", vec_x[i], vec_y[i]);
-		}
-	}*/
-
 	//zeroflag
 	if (b == a) {
 		if (tx == sx) {
 			zeroflag_x = 1;
-			rep(i, 1000000) vec_x.push_back(b);
+			repl(i, maxi) vec_x.push_back(b);
 		}
 		else if ((tx + sx) == 2 * b) {
 			zeroflag_x = 2;
-			rep(i, 1000000 - 1) vec_x.push_back(b);
+			repl(i, maxi - 1) vec_x.push_back(b);
 		}
 		else {
 			cout << "No" << endl; return 0;
@@ -195,60 +134,84 @@ int main() {
 	if (d == c) {
 		if (ty == sy) {
 			zeroflag_y = 1;
-			rep(i, 1000000) vec_y.push_back(d);
+			repl(i, maxi) vec_y.push_back(d);
 		}
 		else if ((ty + sy) == 2 * d) {
 			zeroflag_y = 2;
-			rep(i, 1000000 - 1) vec_y.push_back(d);
+			repl(i, maxi - 1) vec_y.push_back(d);
 		}
 		else {
 			cout << "No" << endl; return 0;
 		}
 	}
 
-	if ((tx - sx) <= 1000000 * (b - a) && (ty-tx) <= 1000000*(d-c) && (zeroflag_x == 0 || zeroflag_x == 1) && (zeroflag_y==0 || zeroflag_y==1)) {//偶数回操作
+	if ((zeroflag_x == 0 || zeroflag_x == 1) && (zeroflag_y == 0 || zeroflag_y == 1)) {//偶数回操作
 		if (zeroflag_x == 0) {
-			ll btoa_x = (tx - sx) / (2 * (b - a));
-			ll mod_x = (tx - sx) % (2 * (b - a)); mod_x /= 2;
-			ll remain_x = 1000000 - 2 - 2 * btoa_x;
-			repl(i, btoa_x) {
-				vec_x.push_back(a); vec_x.push_back(b);
-			}
-			vec_x.push_back(b - mod_x);
-			vec_x.push_back(b);
-			repl(i, remain_x) {
+			if (tx >= sx) {
+				ll btoa_x = (tx - sx) / (2 * (b - a));
+				ll mod_x = (tx - sx) % (2 * (b - a)); mod_x /= 2;
+				ll remain_x = maxi - 2 - 2 * btoa_x;
+				repl(i, btoa_x) {
+					vec_x.push_back(a); vec_x.push_back(b);
+				}
+				vec_x.push_back(b - mod_x);
 				vec_x.push_back(b);
+				repl(i, remain_x) {
+					vec_x.push_back(b);
+				}
+			}
+			else {
+				ll btoa_x = (sx - tx) / (2 * (b - a));
+				ll mod_x = (sx - tx) % (2 * (b - a)); mod_x /= 2;
+				ll remain_x = maxi - 2 - 2 * btoa_x;
+				repl(i, btoa_x) {
+					vec_x.push_back(b); vec_x.push_back(a);
+				}
+				vec_x.push_back(b);
+				vec_x.push_back(b - mod_x);
+				repl(i, remain_x) {
+					vec_x.push_back(b);
+				}
 			}
 		}
 		if (zeroflag_y == 0) {
-			ll btoa_y = (ty - sy) / (2 * (d - c));
-			ll mod_y = (ty - sy) % (2 * (d - c)); mod_y /= 2;
-			ll remain = 1000000 - 2 - 2 * btoa_y;
-			repl(i, btoa_y) {
-				vec_y.push_back(c); vec_y.push_back(d);
-			}
-			vec_y.push_back(d - mod_y);
-			vec_y.push_back(d);
-			repl(i, remain) {
+			if (ty >= sy) {
+				ll btoa_y = (ty - sy) / (2 * (d - c));
+				ll mod_y = (ty - sy) % (2 * (d - c)); mod_y /= 2;
+				ll remain = maxi - 2 - 2 * btoa_y;
+				repl(i, btoa_y) {
+					vec_y.push_back(c); vec_y.push_back(d);
+				}
+				vec_y.push_back(d - mod_y);
 				vec_y.push_back(d);
+				repl(i, remain) {
+					vec_y.push_back(d);
+				}
+			}
+			else {
+				ll btoa_y = (sy - ty) / (2 * (d - c));
+				ll mod_y = (sy - ty) % (2 * (d - c)); mod_y /= 2;
+				ll remain = maxi - 2 - 2 * btoa_y;
+				repl(i, btoa_y) {
+					vec_y.push_back(d); vec_y.push_back(c);
+				}
+				vec_y.push_back(d);
+				vec_y.push_back(d - mod_y);
+				repl(i, remain) {
+					vec_y.push_back(d);
+				}
 			}
 		}
 	}
-	else if ((tx + sx) <= 1000000 * b - 499999 * 2 * a && (ty + sy) <= 1000000 * d - 499999 * 2 * c && (zeroflag_x == 0 || zeroflag_x == 2) && (zeroflag_y==0 || zeroflag_y==2)) {//奇数回操作
+	else if ((zeroflag_x == 0 || zeroflag_x == 2) && (zeroflag_y == 0 || zeroflag_y == 2)) {//奇数回操作
 		if (zeroflag_x == 0) {
 			ll btoa_x = (tx + sx) / (2 * (b - a));
 			ll mod_x = (tx + sx) % (2 * (b - a)); mod_x /= 2;
 			ll count_x = 0;
 			while (mod_x < a) {
-				mod_x += b - a; count_x++;
+				mod_x += b - a; count_x += (ll)1;
 			}
-			if (btoa_x >= count_x) {
-				count_x = 0; btoa_x -= count_x;
-			}
-			else {
-				btoa_x = 0; count_x -= btoa_x;
-			}
-			ll remain = 1000000 - 1 - 1 - 2 * btoa_x - 2 * count_x;
+			ll remain = maxi - 1 - 1 - 2 * btoa_x - 2 * count_x;
 			vec_x.push_back(mod_x);
 			repl(i, btoa_x) {
 				vec_x.push_back(a); vec_x.push_back(b);
@@ -265,16 +228,10 @@ int main() {
 			ll mod_y = (ty + sy) % (2 * (d - c)); mod_y /= 2;
 			ll count_y = 0;
 			while (mod_y < c) {
-				mod_y += d - c; count_y++;
+				mod_y += d - c; count_y += (ll)1;
 			}
-			if (btoa_y >= count_y) {
-				count_y = 0; btoa_y -= count_y;
-			}
-			else {
-				btoa_y = 0; count_y -= btoa_y;
-			}
-			ll remain = 1000000 - 1 - 1 - 2 * btoa_y - 2 * count_y;
-			vec_y.push_back(count_y);
+			ll remain = maxi - 1 - 1 - 2 * btoa_y - 2 * count_y;
+			vec_y.push_back(mod_y);
 			repl(i, btoa_y) {
 				vec_y.push_back(c); vec_y.push_back(d);
 			}
@@ -291,7 +248,7 @@ int main() {
 	}
 
 	cout << "Yes" << endl;
-	rep(i, vec_x.size()) {
+	rep(i, max(vec_x.size(), vec_y.size())) {
 		cout << vec_x[i] << " " << vec_y[i] << endl;
 	}
 
