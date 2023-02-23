68b502b7f27fc1030406cd849b453790eda5303a
KeitoKubo <89447369+KeitoKubo@users.noreply.github.com>
2023-02-24 00:03:57 +0900

    changed directory

---
+++ Pokemon.cpp
@@ -0,0 +1,151 @@
+#define _USE_MATH_DEFINES
+#include <iostream>                         // cout, endl, cin
+#include <cstdio>                           // printf, scanf
+#include <string>                           // string, to_string, stoi
+#include <algorithm>                        // min, max, swap, lower_bound, upper_bound, stable_sort, sort, reverse
+#include <cmath>                            //sin(rad),cos,tan, abs, pow, sqrt, cbrt,  exp, log, log10, log2
+#include <utility>                          // pair, make_pair
+#include <map>                              // map
+#include <set>                              // set
+#include <vector>                           // vector
+#include <queue>                            // queue, priority_queue
+#include <stack>                            // stack
+#include <list>                             // list
+#include <deque>                            // deque
+#include <iomanip>
+#include <numeric>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef vector<int> vi;
+typedef vector<string> vs;
+typedef pair<int, int> pii;
+typedef pair<string, int> psi;
+typedef pair<int, string> pis;
+typedef set<int> si;
+typedef map<string, int> msi;
+typedef map<string, bool> msb;
+typedef priority_queue<int> pqi;
+typedef stack<int> sti;
+typedef queue<int> qi;
+
+#define infi 2147483647
+#define infl 9223372036854775806
+#define MOD  1000000007
+#define EPS 0.0000000001
+#define rep(i,n) for(int i=0;i<(int)n;i++)
+#define repa(i,n) for(int i=1;i<=(int)n;i++)
+#define irep(i,n) for(int i=(int)n-1;i>=0;i--)
+#define repl(i,n) for(ll i=0;i<(ll)n;i+=(ll)1)
+
+//Union Find Tree
+class DisjointSet {
+public:
+	vi rank, p, child; //rankはその木の深さ、pは親ノード
+
+	DisjointSet() {}
+	DisjointSet(int size) {
+		rank.resize(size + 1, 0);
+		p.resize(size + 1, 0);
+		child.resize(size + 1, 0);
+		repa(i, size) makeSet(i);
+	}
+
+	void makeSet(int x) {
+		p[x] = x;
+		rank[x] = 0;
+		child[x] = 1;
+	}
+
+	int findSet(int x) {
+		if (x != p[x]) p[x] = findSet(p[x]);
+		return p[x];
+	}
+
+	int howmanychilds(int x) {
+		return child[x];
+	}
+
+	bool same(int x, int y) {
+		if (findSet(x) == findSet(y)) return true;
+		else return false;
+	}
+
+	void link(int x, int y) {
+		if (rank[x] > rank[y]) {
+			p[y] = x;
+			child[x] += child[y];
+		}
+		else {
+			p[x] = y; child[y] += child[x];
+			if (rank[x] == rank[y]) rank[y]++;
+		}
+	}
+
+	void unite(int x, int y) {
+		link(findSet(x), findSet(y));
+	}
+};
+
+//sort vector<pii> by second element
+bool compare_by_b(pair<int, int> a, pair<int, int> b) {
+	if (a.second != b.second) {
+		return a.second < b.second;
+	}
+	else {
+		return a.first < b.first;
+	}
+}
+
+//---------------------------------------------------
+void solve_1() {//O(N)
+	int n; ll x, y;
+	cin >> n >> x >> y;
+	ll ans = 0;
+	rep(i, n) {
+		ll p, q; scanf("%d%d", &p, &q);
+		ans += q - p;
+	}
+	cout << ans << endl;
+}
+
+void solve_2() { //O(NlogN)
+	int n; ll x, y;
+	cin >> n >> x >> y;
+	vector<ll> lvl;
+	rep(i, n) {
+		ll p, q; scanf("%d%d", &p, &q);
+		lvl.push_back(q - p);
+	}
+	sort(lvl.begin(), lvl.end());
+
+	//yをlvl[0]回行う
+	ll ans = lvl[0];
+	rep(i, lvl.size()) lvl[i] -= lvl[0];
+	
+}
+
+void solve_3() {
+
+}
+
+int main() {
+	int step; cin >> step;
+	if (step == 1) {
+		solve_1();
+	}
+	else if (step == 2) {
+		solve_2();
+	}
+	else {
+		solve_3();
+	}
+
+	return 0;
+}
+
