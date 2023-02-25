aa434bf1b7736ab037e34f62e6407258f7ebd4f2
KeitoKubo <89447369+KeitoKubo@users.noreply.github.com>
2023-02-25 18:35:55 +0900

    POJ2686

---
+++ AtCoder/Typical Problems/DP/BitDP_POJ2686.cpp
@@ -0,0 +1,159 @@
+#define _USE_MATH_DEFINES
+#include <iostream>                         // cout, endl, cin
+#include <cstdio>                           // printf, scanf
+#include <string>                           // string, to_string, stoi
+#include <algorithm>                        // min, max, swap, lower_bound, upper_bound
+// & stable_sort, sort, reverse
+#include <cmath>                            //sin(rad),cos,tan, abs, pow, sqrt, cbrt,  exp, log, log10, log2
+#include <utility>                          // pair, make_pair
+#include <map>                              // map
+#include <set>                              // set
+#include <vector>                           // vector
+#include <queue>                            // queue, priority_queue
+#include <stack>                            // stack
+#include <list>                             // list
+#include <deque>                            // deque
+#include <iomanip>
+#include <numeric>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef vector<int> vi;
+typedef vector<string> vs;
+typedef pair<int, int> pii;
+typedef pair<string, int> psi;
+typedef pair<int, string> pis;
+typedef set<int> si;
+typedef map<string, int> msi;
+typedef map<string, bool> msb;
+typedef priority_queue<int> pqi;
+typedef stack<int> sti;
+typedef queue<int> qi;
+
+#define infi 2147483647
+#define infl 9223372036854775806
+#define MOD  1000000007
+#define EPS 0.0000000001
+#define rep(i,n) for(int i=0;i<(int)n;i++)
+#define repa(i,n) for(int i=1;i<=(int)n;i++)
+#define irep(i,n) for(int i=(int)n-1;i>=0;i--)
+
+//Union Find Tree
+class DisjointSet {
+public:
+	vi rank, p, child; //rankはその木の深さ、pは親ノード
+
+	DisjointSet() {}
+	DisjointSet(int size) {
+		rank.resize(size + 1, 0);
+		p.resize(size + 1, 0);
+		child.resize(size + 1, 0);
+		repa(i, size) makeSet(i);
+	}
+
+	void makeSet(int x) {
+		p[x] = x;
+		rank[x] = 0;
+		child[x] = 1;
+	}
+
+	int findSet(int x) {
+		if (x != p[x]) p[x] = findSet(p[x]);
+		return p[x];
+	}
+
+	int howmanychilds(int x) {
+		return child[x];
+	}
+
+	bool same(int x, int y) {
+		if (findSet(x) == findSet(y)) return true;
+		else return false;
+	}
+
+	void link(int x, int y) {
+		if (rank[x] > rank[y]) {
+			p[y] = x;
+			child[x] += child[y];
+		}
+		else {
+			p[x] = y; child[y] += child[x];
+			if (rank[x] == rank[y]) rank[y]++;
+		}
+	}
+
+	void unite(int x, int y) {
+		link(findSet(x), findSet(y));
+	}
+};
+
+//sort vector<pii> by second element
+bool compare_by_b(pair<int, int> a, pair<int, int> b) {
+	if (a.second != b.second) {
+		return a.second < b.second;
+	}
+	else {
+		return a.first < b.first;
+	}
+}
+
+//---------------------------------------------------
+const int MX = 31;
+const int IMPOS = 1000000;
+int n, m, p, a, b;
+double t[10];
+double e[MX][MX];
+double dp[1 << 8][MX];
+
+int main() {
+	//(void)scanf("%d",& );
+	//(void)scanf("%d%d",& ,& );
+	cin >> n >> m >> p >> a >> b;
+	--a; --b;
+	rep(i, n) cin >> t[i];
+	rep(i, m) { rep(j, m) e[i][j] = -1; }
+	rep(i, p) {
+		int x, y; double z; cin >> x >> y >> z;
+		--x; --y;
+		e[x][y] = z; e[y][x] = z;
+	}
+
+	rep(i, 1 << n) {
+		rep(j, MX) {
+			dp[i][j] = IMPOS;
+		}
+	}
+	dp[0][a] = 0;
+
+	for (int i = 0; i <= (1 << n) - 2; i++) {
+		rep(v, m) {
+			if (dp[i][v] != IMPOS) {
+				rep(u, m) { //頂点uへ移動
+					if (v != u && e[v][u] > 0) { //vからuへ辺が存在する
+						rep(k, n) { //k番目のチケット使用
+							if (!((i >> k) & 1)) { //使用されていない時のみ
+								dp[i | (1 << k)][u] = min(dp[i | (1 << k)][u], dp[i][v] + e[v][u] / t[n - 1 - k]);
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+
+	double ans = IMPOS;
+	rep(i, (1 << n)) {
+		ans = min(ans, dp[i][b]);
+	}
+	if (ans == IMPOS) cout << "Impossible" << endl;
+	else cout << ans << endl;
+
+
+	return 0;
+}
+
